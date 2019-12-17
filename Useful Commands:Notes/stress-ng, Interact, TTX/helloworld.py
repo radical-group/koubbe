f93fba50c9c0a72232308082ce306fcd3da07310
George Koubbe <gkoubbe@outlook.com>
2019-12-17 15:56:03 -0500

    Weekly Summary 12 and Container Experiments Design

---
+++ Useful Commands:Notes/stress-ng, Interact, TTX/helloworld.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Parallel Hello World
+"""
+
+from mpi4py import MPI
+import sys
+import time
+import os
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+
+print("Hello, World! I am process %d of %d on %s.\n" % (rank, size, name))
+
+#time.sleep(100)
+os.system('/home/karahbit/stress-ng-0.10.10/./stress-ng -c 2 -t 100')
